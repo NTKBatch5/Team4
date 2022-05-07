ff29fde3b3df3665c5a6f0c8f6f16ad22f4c6add
milot <milot@DESKTOP-1PJF4EO>
2022-05-07 10:53:05 -0400

    Milotina changes

--- src/test/java/demo/DemoTest.java
+++ src/test/java/demo/DemoTest.java
@@ -4,11 +4,8 @@ public class DemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Test");
-		addNum();
 	}
-	public static void addNum()
-	{
-		System.out.println("Changes by Addy");
-	}
+	
+	//Milotina
 
 }
